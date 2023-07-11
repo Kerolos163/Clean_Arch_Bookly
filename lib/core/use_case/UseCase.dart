f03f87fc78443a1981bb78241a96621707cf71bc
Kerolos163 <kokoessa951@gmail.com>
2023-07-11 20:08:32 +0300

    Refactor use case class

---
+++ lib/core/use_case/UseCase.dart
@@ -0,0 +1,8 @@
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UseCase<Type, param> {
+  Future<Either<Failures, Type>> call([param param]);
+}
+
+class No_Param {}
