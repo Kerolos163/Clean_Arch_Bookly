dbd681158ca2e5d116adc050c7997cf322cdcf5e
Kerolos163 <kokoessa951@gmail.com>
2023-07-11 18:30:27 +0300

    add either class

--- lib/features/home/Domain/repos/HomeRepo.dart
+++ lib/features/home/Domain/repos/HomeRepo.dart
@@ -1,8 +1,8 @@
+import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/features/home/Domain/Entities/Book_Entitiy.dart';
+import 'package:dartz/dartz.dart';
 
-abstract class HomeRepo
-{
-  Future<List<BookEntity>> FetchFeatureBooks();
-  Future<List<BookEntity>> FetchNewestBooks();
-
+abstract class HomeRepo {
+  Future<Either<Failures, List<BookEntity>>> FetchFeatureBooks();
+  Future<Either<Failures, List<BookEntity>>> FetchNewestBooks();
 }
