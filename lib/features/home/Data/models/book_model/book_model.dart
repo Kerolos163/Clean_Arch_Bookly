710922ba58815777de1ce7d6e31a164fabf1b035
Kerolos163 <kokoessa951@gmail.com>
2023-07-12 05:01:58 +0300

    Create BookModel extends BookEntity

--- lib/features/home/Data/models/book_model/book_model.dart
+++ lib/features/home/Data/models/book_model/book_model.dart
@@ -1,11 +1,12 @@
 import 'package:equatable/equatable.dart';
 
+import '../../../Domain/Entities/Book_Entitiy.dart';
 import 'access_info.dart';
 import 'sale_info.dart';
 import 'search_info.dart';
 import 'volume_info.dart';
 
-class BookModel extends Equatable {
+class BookModel extends BookEntity {
   final String? kind;
   final String? id;
   final String? etag;
@@ -15,7 +16,7 @@ class BookModel extends Equatable {
   final AccessInfo? accessInfo;
   final SearchInfo? searchInfo;
 
-  const BookModel({
+  BookModel({
     this.kind,
     this.id,
     this.etag,
@@ -24,7 +25,13 @@ class BookModel extends Equatable {
     this.saleInfo,
     this.accessInfo,
     this.searchInfo,
-  });
+  }) : super(
+            bookid: id!,
+            image: volumeInfo.imageLinks?.thumbnail ??"",
+            authorName: volumeInfo.authors!.first,
+            price: 0,
+            title: volumeInfo.title ?? "No Name",
+            rating: volumeInfo.averageRating);
 
   factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
         kind: json['kind'] as String?,
