974439c11a79af3790fc1c521a5aa5b52d4c5d20
Kerolos163 <kokoessa951@gmail.com>
2023-07-19 05:35:57 +0300

    register Adapter & Opren Feature Box

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 
 
+import 'package:bookly_app/features/home/Domain/Entities/Book_Entity.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
 
 import 'constants.dart';
 import 'core/utils/app_router.dart';
@@ -13,9 +15,12 @@ import 'features/home/presentation/view_models/Newest_Books_Cubit/cubit.dart';
 import 'features/search/Data/search_repoimp.dart';
 import 'features/search/presentation/views/view_model/Search_cubit/cubit.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   SetupServiceLocator();
+  await Hive.initFlutter();
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox<BookEntity>(kFeatureBox);
   runApp(const Bookly());
 }
 
