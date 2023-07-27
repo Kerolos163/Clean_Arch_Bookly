0adc769c0a78c8a0f65a8e9e57003d8290dfac04
Kerolos163 <kokoessa951@gmail.com>
2023-07-27 04:28:26 +0300

    Add Scroll to 70%

--- lib/features/home/presentation/views/Widget/home_body.dart
+++ lib/features/home/presentation/views/Widget/home_body.dart
@@ -8,12 +8,36 @@ import 'List_view.dart';
 import 'best_seller_List_Item.dart';
 import 'customappbar.dart';
 
-class HomeViewBody extends StatelessWidget {
+class HomeViewBody extends StatefulWidget {
   const HomeViewBody({Key? key});
+
+  @override
+  State<HomeViewBody> createState() => _HomeViewBodyState();
+}
+
+class _HomeViewBodyState extends State<HomeViewBody> {
+  final _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    _scrollController.addListener(_onScroll);
+    super.initState();
+  }
+
+  void _onScroll() {
+    final double currentPosition = _scrollController.position.pixels;
+    final double maxExtent = _scrollController.position.maxScrollExtent;
+    final double percentage = currentPosition / maxExtent;
+    if (percentage >= 0.7) {
+      // user has reached 70% of the CustomScrollView
+      print('User has reached 70%');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
+      controller: _scrollController,
       physics: const BouncingScrollPhysics(),
       slivers: [
         SliverToBoxAdapter(
